f22d1411ad0bb40585803d787e632e4ed4f2486a
Karolina Krzysik <79480965+KarolinaKrzysik@users.noreply.github.com>
2021-02-26 23:33:37 +0000

    Task 1 from week 2

---
+++ wk2ex1.php
@@ -0,0 +1,12 @@
+<?php 
+    $hourlyRate = 5.75;
+    $hoursPerWeek = 40;
+    $gross = $hourlyRate * $hoursPerWeek;
+?>
+<html>
+<head>
+</head>
+<body>
+    <p>My gross wage is <?php print $gross; ?></p>
+</body>
+</html>
